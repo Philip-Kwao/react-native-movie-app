3fd532f70f17930ea1c0c45dd5cecce0d1ed4dd8
Philip-Kwao <kwaoizy@gmail.com>
2025-08-27 20:38:54 +0000

    Implemented Routing between 4 tabs && added asset files

---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const movieDetail = () => {
+  return (
+    <View>
+      <Text>movieDetail</Text>
+    </View>
+  )
+}
+
+export default movieDetail
+
+const styles = StyleSheet.create({})
