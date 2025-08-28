c3d622f065c9e2a11894408139e612431add9bab
Philip-Kwao <kwaoizy@gmail.com>
2025-08-28 00:14:37 +0000

    Created customized hook (useFetch) to fetch data in different instances

---
+++ app/services/useFetch.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+
+const useFetch = <T>(fetchFunction:()=>Promise<T>,autoFetch=true)=>{
+    const [data, setData] = useState<T | null>(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState<Error | null>(null)
+
+    const fetchData = async()=>{
+        try{
+            setLoading(true)
+            setError(null)
+
+            const result = await fetchFunction()
+            setData(result)
+        }
+        catch(err){
+            setError(err instanceof Error ? err : new Error("An Error Occured"))
+        }
+        finally{
+            setLoading(false)
+        }
+        // Reset State
+        const reset = ()=>{
+            setData(null)
+            setLoading(false)
+            setError(null)
+        }
+
+        // Call Function at the beginning of the program
+        useEffect(()=>{
+            if(autoFetch){
+                fetchData()
+            }
+        },[])
+
+        return {data, loading, error, refetch:fetchData, reset}
+    }
+
+}
+
+export default useFetch
